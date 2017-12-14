5c9e5af6df34672f660a2ed2a75d33b6cc45bfe5
RohanGada <rohangadaconsultant@localhost>
2017-12-14 15:46:29 +0530

    First commit

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,19 @@
 import { StackNavigator } from 'react-navigation'
+import SearchScreen from '../Containers/SearchScreen'
+import TextableScreen from '../Containers/TextableScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  SearchScreen: { screen: SearchScreen },
+  TextableScreen: { screen: TextableScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+initialRouteName : 'TextableScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
